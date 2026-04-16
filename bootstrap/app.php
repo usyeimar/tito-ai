f771004fb3fbe28b759c84ee537c3fca73813253
Yeimar Yecid Lemus Romaña <yeimar112003@outlook.com>
2026-04-16 13:19:57 -0500

    Trust all proxies for request IP detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/health',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
