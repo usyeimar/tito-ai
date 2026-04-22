682a6c9ed9cc38891ebd357b74b9fab004183241
Yeimar Yecid Lemus Romaña <yeimar112003@outlook.com>
2026-04-21 22:06:08 -0500

    refactor: remove unused legacy policies

--- app/Policies/ResourcesPolicy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Policies;
-
-class ResourcesPolicy extends ModulePolicy
-{
-    protected string $module = 'resources';
-}
--- app/Policies/RingCentralPolicy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Policies;
-
-class RingCentralPolicy extends ModulePolicy
-{
-    protected string $module = 'ring_central';
-}
