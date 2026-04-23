5292a7ede39d99b614647b709c284d6181972ef1
Yeimar Yecid Lemus Romaña <yeimar112003@outlook.com>
2026-04-22 21:57:12 -0500

    chore(testing): configure multi-database testing environment
    .env.testing:
    - Rename test database to testing_app
    - Add DB_CONNECTION_CENTRAL and DB_DATABASE_CENTRAL
    
    docker/pgsql/create-testing-database.sql:
    - Update database name from testing to testing_app
    
    phpunit.xml:
    - Add force="true" to all database env variables
    - Add DB_CONNECTION_CENTRAL and DB_DATABASE_CENTRAL with force="true"
    - Ensure test database configuration overrides .env values

--- docker/pgsql/create-testing-database.sql
+++ docker/pgsql/create-testing-database.sql
@@ -1,2 +1,2 @@
-SELECT 'CREATE DATABASE testing'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testing')\gexec
+SELECT 'CREATE DATABASE testing_app'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testing_app')\gexec
